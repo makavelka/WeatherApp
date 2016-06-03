cd9b4a1ef76c09151b83f77554196932fb747e79
makavelka <vovchilnik@yandex.ru>
2016-06-03 12:25:29 +0300

    Interface with const

--- app/src/main/java/com/example/weatherapp/Const.java
+++ app/src/main/java/com/example/weatherapp/Const.java
@@ -2,4 +2,7 @@ package com.example.weatherapp;
 
 public interface Const {
     String WEATHER_API_KEY = "8278cb9d20902f4ec47ce3eec5dffdd1";
+    String BASE_URL = "http://api.openweathermap.org/data/2.5/";
+    String UI_THREAD = "UI_THREAD";
+    String IO_THREAD = "IO_THREAD";
 }
