bb542e57502a2cf2c4b944b95cf4684a3b2afb9e
makavelka <vovchilnik@yandex.ru>
2016-06-03 13:16:50 +0300

    Get data from internet

--- app/src/main/java/com/example/weatherapp/model/Model.java
+++ app/src/main/java/com/example/weatherapp/model/Model.java
@@ -1,8 +1,14 @@
 package com.example.weatherapp.model;
 
+import com.example.weatherapp.model.pojo.CurrentWeather;
+import com.example.weatherapp.model.pojo.FiveDaysWeather;
+
+import rx.Observable;
+
 public interface Model {
     void getDataFromDb();
-    void getDataFromInternet();
+    Observable<CurrentWeather> getCurrentWeather(String city);
+    Observable<FiveDaysWeather> getFiveDaysWeather(String city);
     void getData();
     boolean checkIfDataExistInDb();
 }
--- app/src/main/java/com/example/weatherapp/model/ModelImpl.java
+++ app/src/main/java/com/example/weatherapp/model/ModelImpl.java
@@ -1,6 +1,33 @@
 package com.example.weatherapp.model;
 
+import com.example.weatherapp.Const;
+import com.example.weatherapp.di.App;
+import com.example.weatherapp.model.api.ApiInterface;
+import com.example.weatherapp.model.pojo.CurrentWeather;
+import com.example.weatherapp.model.pojo.FiveDaysWeather;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import rx.Observable;
+import rx.Scheduler;
+
 public class ModelImpl implements Model {
+
+    @Inject
+    protected ApiInterface apiInterface;
+
+    @Inject
+    @Named(Const.UI_THREAD)
+    Scheduler uiThread;
+
+    @Inject
+    @Named(Const.IO_THREAD)
+    Scheduler ioThread;
+
+    public ModelImpl() {
+        App.getComponent().inject(this);
+    }
 
     @Override
     public void getDataFromDb() {
@@ -8,8 +35,17 @@ public class ModelImpl implements Model {
     }
 
     @Override
-    public void getDataFromInternet() {
+    public Observable<CurrentWeather> getCurrentWeather(String city) {
+        return apiInterface.getWeather(city)
+                .subscribeOn(ioThread)
+                .observeOn(uiThread);
+    }
 
+    @Override
+    public Observable<FiveDaysWeather> getFiveDaysWeather(String city) {
+        return apiInterface.getWeatherForFiveDays(city)
+                .subscribeOn(ioThread)
+                .observeOn(uiThread);
     }
 
     @Override
